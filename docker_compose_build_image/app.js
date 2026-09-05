37e894fc318d7ccea50146b3913f9ffe1b6e0bf0
Quang153636 <quang153636@gmail.com>
2026-09-05 16:40:35 +0000

    Log NODE_ENV along with app name and environment

--- docker_compose_build_image/app.js
+++ docker_compose_build_image/app.js
@@ -1,5 +1,5 @@
 // app.js
 const appName = process.env.APP_NAME || 'Unknown';
 const environment = process.env.ENVIRONMENT || 'Unknown';
-
-console.log(`Running ${appName} in ${environment} environment.`);
+const node_evn = process.env.NODE_ENV || 'Unknown';
+console.log(`Running ${appName} in ${environment} environment and ${node_env}`);
